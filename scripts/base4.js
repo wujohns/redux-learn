5be931a94b295a9a3b6e6880b66d0f606b915b2f
wujohns <wujohns@163.com>
2017-06-20 18:41:02 +0800

    依据难易度进行了调整

--- scripts/base4.js
+++ scripts/base4.js
@@ -7,3 +7,37 @@
 'use strict';
 
 // applyMiddleware 的原始代码
+const applyMiddleware = (...middlewares) => {
+    return (createStore) => {
+        return (reducer, preloadedState, enhancer) => {
+            const store = createStore(reducer, preloadedState, enhancer);
+            let dispatch = store.dispatch;
+            let chain = [];
+
+            const middlewareAPI = {
+                getState: store.getState,
+                dispatch: (action) => dispatch(action)
+            };
+            chain = middlewares.map(middleware => middleware(middlewareAPI));
+            dispatch = compose(...chain)(store.dispatch);
+
+            return {
+                ...store,
+                dispatch
+            }
+        }
+    };
+};
+
+// redux-thunk 原始代码，thunk 即为该中间件
+const createThunkMiddleware = (extraArgument) => {
+    return ({ dispatch, getState }) => next => action => {
+        if (typeof action === 'function') {
+            return action(dispatch, getState, extraArgument);
+        }
+        return next(action);
+    };
+};
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
